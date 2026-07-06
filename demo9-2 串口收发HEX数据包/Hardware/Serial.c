f6756a66dd039588d84fba9b12c086583d2b386e
xudxus <xudxus145@gmail.com>
2026-07-06 21:26:52 +0800

    更新串口收发HEX数据包代码

---
+++ demo9-2 串口收发HEX数据包/Hardware/Serial.c
@@ -0,0 +1,168 @@
+#include "stm32f10x.h"                  // Device header
+#include <stdio.h>
+#include "OLED.h"
+
+uint8_t Serial_TxPacket[4];
+uint8_t Serial_RxPacket[4];
+uint8_t Serial_RxFlag;
+
+void Serial_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+  	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 ;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//硬件流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;//串口模式,接接收发送之间用或间隔
+	USART_InitStructure.USART_Parity = USART_Parity_No;//校验模式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//停止位位长
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART1,&USART_InitStructure);
+	
+	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);//开启中断接收
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStructure);
+
+	USART_Cmd(USART1,ENABLE);
+}
+
+
+void Serial_SendByte(uint8_t byte)
+{
+	USART_SendData(USART1,byte);
+	while (USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET);
+}
+
+void Serial_SendArray(uint8_t *Array,uint16_t Length)
+{
+	uint16_t i;
+	for(i = 0;i < Length;i++)
+	{
+		Serial_SendByte(Array[i]);
+	}
+}
+
+void Serial_SendString(char *String)
+{
+	uint8_t i;
+	for(i = 0;String[i] != '\0';i++)
+	{
+		Serial_SendByte(String[i]);
+	}
+}
+
+uint32_t Serial_Pow(uint32_t X,uint32_t Y)
+{
+	uint32_t Result = 1;
+	while (Y--)                           //--Y
+	{
+		Result *= X;                //result = result * x
+	}
+	return Result;
+}
+
+void Serial_SendNumber(uint32_t Number,uint8_t Length)
+{
+	uint8_t i;
+	for(i = 0;i < Length;i++)
+	{
+		Serial_SendByte(Number / Serial_Pow(10,Length - i - 1) % 10 + '0');
+	}
+}
+
+int fputc(int ch,FILE *f)
+{
+	Serial_SendByte(ch);
+	return ch;
+}
+
+void Serial_ReceiveData(void)
+{
+	while(1)
+	{
+		uint16_t RxData;
+		if(USART_GetFlagStatus(USART1,USART_FLAG_RXNE) == SET)
+		{
+			RxData = USART_ReceiveData(USART1);
+			OLED_ShowHexNum(1,1,RxData,2);
+		}
+	}
+}
+
+void Serial_SendPacket(void)
+{
+	Serial_SendByte(0xFF);
+	Serial_SendArray(Serial_TxPacket,4);
+	Serial_SendByte(0xFE);
+
+}
+
+uint8_t Serial_GetRxFlag(void)
+{
+	if(Serial_RxFlag == 1)
+	{
+		Serial_RxFlag = 0;
+		return 1;
+	}
+	return 0;
+}
+
+
+
+void USART1_IRQHandler(void)
+{
+	static uint8_t RxState = 0;
+	static uint8_t pRxPacket = 0;
+
+	if (USART_GetITStatus(USART1,USART_IT_RXNE) == SET)
+	{
+		
+		uint8_t RxData = USART_ReceiveData(USART1);
+		
+		if (RxState == 0)
+		{
+			if(RxData == 0xFF)
+			{
+				RxState = 1;
+			}
+		}
+		else if(RxState == 1)
+		{
+			Serial_RxPacket[pRxPacket] = RxData;
+			pRxPacket++;
+			if(pRxPacket >= 4)
+			{
+				RxState = 2;
+			}
+		}
+		else if(RxState == 2)
+		{
+			if(RxData == 0xFE)
+			{
+				RxState = 0;
+				Serial_RxFlag = 1;
+			}
+		}
+		USART_ClearITPendingBit(USART1,USART_IT_RXNE);//清除标志位
+	}
+}
+
