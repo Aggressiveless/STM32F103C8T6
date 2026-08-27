590c054220c65c0267cd4c239bb678015864efda
xudxus <xudxus145@gmail.com>
2026-08-27 19:39:25 +0800

    更新硬件电路连接，将OLED上的sck sda口分别接一个4.7k欧的上拉电阻至3.3v，现阶段完美解决了OLED花屏黑屏现象，同时优化转速设置代码，防止满转速情况下电机驱动的外置供电口电流过大过载运行导致过载保护断电，下一阶段即开始正式的PID控制，加快到达目标值，减缓超出值出现

--- demo9-x 智能温控风扇系统/demo9-x1 串口发送温控信号+PWM占空比/Hardware/Motor.c
+++ demo9-x 智能温控风扇系统/demo9-x1 串口发送温控信号+PWM占空比/Hardware/Motor.c
@@ -25,17 +25,18 @@ void Motor_Init(void)
 
 void Motor_SetSpeed(int16_t Speed)
 {
+	int16_t ActualSpeed = Speed * 0.6; //避免峰值电流过大导致转接器电流过载自动断电
 	if(Speed >= 0)
 	{
 		GPIO_SetBits(GPIOA,GPIO_Pin_5);
 		GPIO_ResetBits(GPIOA,GPIO_Pin_4);
-		PWM_SetCompare3(Speed);
+		PWM_SetCompare3(ActualSpeed);
 	}
 	else
 	{
 		GPIO_SetBits(GPIOA,GPIO_Pin_4);
 		GPIO_ResetBits(GPIOA,GPIO_Pin_5);
-		PWM_SetCompare3(-Speed);
+		PWM_SetCompare3(-ActualSpeed);
 	}
 }
 
